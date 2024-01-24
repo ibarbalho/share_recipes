a388db487434ebf8587944d6ec7a60db7ecfed7f
Ittalo Barbalho <barbalho@etamax.de>
2024-01-24 11:25:50 +0100

    Remove security login

--- src/main/java/com/barbalho/configuration/AppConfig.java
+++ src/main/java/com/barbalho/configuration/AppConfig.java
@@ -1,54 +1,39 @@
 package com.barbalho.configuration;
 
-import java.util.Collections;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-@Configuration
-@EnableMethodSecurity
-public class AppConfig {
-	
-	@Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
-		
-		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-		.authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/**")
-				.authenticated().anyRequest().permitAll())
-		.csrf(csrf -> csrf.disable())
-		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
-		.formLogin(Customizer.withDefaults());
-		
-		return http.build();
-	}
-
-	private CorsConfigurationSource corsConfigurationSource() {		
-		
-		return new CorsConfigurationSource() {
-			
-			@Override
-			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-				CorsConfiguration cfg = new CorsConfiguration();
-				cfg.setAllowedOrigins(Collections.singletonList("*"));
-				cfg.setAllowedMethods(Collections.singletonList("*"));
-				cfg.setAllowedHeaders(Collections.singletonList("*"));
-				cfg.setExposedHeaders(Collections.singletonList("*"));
-				cfg.setMaxAge(3600L);
-				return cfg;
-			}
-		};
-	}
+//@Configuration
+//@EnableMethodSecurity
+//public class AppConfig extends WebSecurityConfiguration{
+//	
+//	@Bean
+//	SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+//		
+//		http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//		.authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/**")
+//				.authenticated().anyRequest().permitAll())
+//		.csrf(csrf -> csrf.disable())
+//		.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+//		.formLogin(Customizer.withDefaults());
+//		
+//		return http.build();
+//	}
+//
+//	private CorsConfigurationSource corsConfigurationSource() {		
+//		
+//		return new CorsConfigurationSource() {
+//			
+//			@Override
+//			public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//				CorsConfiguration cfg = new CorsConfiguration();
+//				cfg.setAllowedOrigins(Collections.singletonList("*"));
+//				cfg.setAllowedMethods(Collections.singletonList("*"));
+//				cfg.setAllowedHeaders(Collections.singletonList("*"));
+//				cfg.setExposedHeaders(Collections.singletonList("*"));
+//				cfg.setMaxAge(3600L);
+//				return cfg;
+//			}
+//		};
+//	}
 
 	
 	
-}
+//}
